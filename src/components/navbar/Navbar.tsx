7e3725b8fbc12a3439cb32bca932fc0aeed2a87c
neilnicky <neilnicholaskanat@gmail.com>
2025-01-21 18:44:04 +0530

    feat: individual pages for section

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -8,33 +8,30 @@ export default function Navbar() {
     <nav className=" w-full px-9  sticky top-0 bg-white shadow-md  py-2  z-30">
       <div className="flex items-center justify-between lg:justify-around ">
         <a href="/">
-          <Image src={logo} alt="more copy systems logo" width={2880} height={1440} className="w-[140px]" />
+          <Image
+            src={logo}
+            alt="more copy systems logo"
+            width={2880}
+            height={1440}
+            className="w-[140px]"
+          />
         </a>
         <div className=" gap-8 hidden md:flex">
           <a href="/" className="text-black/80  hover:text-blue-500 ">
             Home
           </a>
-          <a
-            href="#aboutus"
-            className="text-black/80  hover:text-blue-500 "
-          >
+          <a href="aboutus" className="text-black/80  hover:text-blue-500 ">
             About
           </a>
-          <a
-            href="#products"
-            className="text-black/80  hover:text-blue-500 "
-          >
+          <a href="products" className="text-black/80  hover:text-blue-500 ">
             Products
           </a>
-          <a
-            href="#contact"
-            className="text-black/80  hover:text-blue-500 "
-          >
+          <a href="contact" className="text-black/80  hover:text-blue-500 ">
             Contact
           </a>
         </div>
         <div className="md:hidden">
-          <MobileNavbar/>
+          <MobileNavbar />
         </div>
       </div>
     </nav>
